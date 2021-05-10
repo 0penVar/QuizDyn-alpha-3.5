f655fdc8ab71d473bf5c1145b10d2e7e37510aab
0penVar <71385626+0penVar@users.noreply.github.com>
2021-05-09 21:13:52 -0400

    Update and rename main.c to Calculatrice.c

---
+++ QuizDyn/Calculatrice.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+    int a = 0, b = 0, c = 0, d = 0, e = 0;
+    int f = 0, g = 0, h = 0, i = 0, j = 0;
+    int re
+    
+    printf("Veuillez ajouter votre score : ");
+    scanf("%d", &a);
+  
+    printf("Veuillez ajouter votre score : ");
+    scanf("%d", &b);
+  
+    
+    
+}
