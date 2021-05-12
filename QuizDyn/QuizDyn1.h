b2f2349487c6547200177cfb7af7ac3f9823eda3
0penVar <71385626+0penVar@users.noreply.github.com>
2021-05-12 08:24:44 -0400

    Create QuizDyn1.h

---
+++ QuizDyn/QuizDyn1.h
@@ -0,0 +1,22 @@
+#ifndef
+
+
+#define LIGNE() printf(---------------------------------------------------------------------------------);
+
+#define N() printf("\n");
+#define N2() printf("\n\n");
+
+#define NQUESTION() printf("Quel est la planete la plus eloignee du soleil dans le systeme solaire ?");
+#define NQUESTION() printf("Quel est la plus grosse planete du systeme solaire");
+
+#define N1REPONSE() printf("reponse 1 : ");
+#define N2REPONSE() printf("reponse 2 : ");
+#define N3REPONSE() printf("reponse 3 : ");
+#define N4REPONSE() printf("reponse 4 : ");
+
+#define PRENOM() printf("Quel est votre prenom : ");
+
+#define REFAIRE() printf("Voulez-vous refaire un calcul ?\nSi oui faite 1, si vous voulez sortir de l'application faite 0.");
+
+
+#endif
