3834321c67d7eba58afb70cd41e19a00064fb753
0penVar <71385626+0penVar@users.noreply.github.com>
2021-05-19 08:40:28 -0400

    Update Quiz_Terminal.h

--- QuizDyn/Quiz_Terminal.h
+++ QuizDyn/Quiz_Terminal.h
@@ -2,8 +2,7 @@
 
 
 #define CLS() system("CLS");
-#define END_TASK() printf("Veuillez appuyer sur une touche pour terminer le programme"); \
-                   system("taskkill /IM cb_console_runner.exe");
+#define END_TASK() system("taskkill /IM cb_console_runner.exe");
 
 
 #endif
