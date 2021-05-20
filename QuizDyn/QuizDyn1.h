2a0a13e0a993a2ec1b44e24ae8629090700c5107
0penVar <71385626+0penVar@users.noreply.github.com>
2021-05-20 00:36:25 -0400

    Update QuizDyn1.h

--- QuizDyn/QuizDyn1.h
+++ QuizDyn/QuizDyn1.h
@@ -6,13 +6,125 @@
 #define N() printf("\n");
 #define N2() printf("\n\n");
 
-#define NQUESTION() printf("Quel est la planete la plus eloignee du soleil dans le systeme solaire ?");
-#define NQUESTION() printf("Quel est la plus grosse planete du systeme solaire");
+#define NQ1() printf("Quel est la planete la plus eloignee du soleil dans le systeme solaire ?"); \
+              printf("\nreponse 1 : uranus"); \
+              printf("\nreponse 2 : neptune"); \
+              printf("\nreponse 3 : mercure"); \
+              printf("\nreponse 4 : terre"); \
+              printf("\nreponse 5 : proxima b"); \
+              printf("\nreponse 6 : kepler 186-f"); \
+              printf("\nreponse 7 : venus"); \
+              printf("\nreponse 8 : mars"); \
+              printf("\nreponse 9 : jupiter"); \
+              printf("\nreponse 10 : saturne");
 
-#define N1REPONSE() printf("reponse 1 : ");
-#define N2REPONSE() printf("reponse 2 : ");
-#define N3REPONSE() printf("reponse 3 : ");
-#define N4REPONSE() printf("reponse 4 : ");
+#define NQ2() printf("Quel est la plus grosse planete du systeme solaire ?"); \
+              printf("\nreponse 1 : mercure"); \
+              printf("\nreponse 2 : saturne"); \
+              printf("\nreponse 3 : uranus"); \
+              printf("\nreponse 4 : neptune"); \
+              printf("\nreponse 5 : venus"); \
+              printf("\nreponse 6 : jupiter"); \
+              printf("\nreponse 7 : Sirius A"); \
+              printf("\nreponse 8 : Sirius B"); \
+              printf("\nreponse 9 : terre"); \
+              printf("\nreponse 10 : mars");
+
+#define NQ3() printf("Quel est la plus petite planete du systeme solaire ?"); \
+              printf("\nreponse 1 : "); \
+              printf("\nreponse 2 : "); \
+              printf("\nreponse 3 : "); \
+              printf("\nreponse 4 : "); \
+              printf("\nreponse 5 : "); \
+              printf("\nreponse 6 : "); \
+              printf("\nreponse 7 : "); \
+              printf("\nreponse 8 : "); \
+              printf("\nreponse 9 : "); \
+              printf("\nreponse 10 : ");
+
+#define NQ4() printf("Comment appelons les planetes qui se trouvent dans notre systeme solaire interne ?"); \
+              printf("\nreponse 1 : "); \
+              printf("\nreponse 2 : "); \
+              printf("\nreponse 3 : "); \
+              printf("\nreponse 4 : "); \
+              printf("\nreponse 5 : "); \
+              printf("\nreponse 6 : "); \
+              printf("\nreponse 7 : "); \
+              printf("\nreponse 8 : "); \
+              printf("\nreponse 9 : "); \
+              printf("\nreponse 10 : ");
+
+#define NQ5() printf("Quel est le nom de l'etoile la plus proche du soleil ?"); \
+              printf("reponse 1 : "); \
+              printf("reponse 2 : "); \
+              printf("reponse 3 : "); \
+              printf("reponse 4 : "); \
+              printf("reponse 5 : "); \
+              printf("reponse 6 : "); \
+              printf("reponse 7 : "); \
+              printf("reponse 8 : "); \
+              printf("reponse 9 : "); \
+              printf("reponse 10 : ");
+
+#define NQ6() printf("Quel autre nom donnent-ont au virus informatique celebre qui se nomme le virus tchernobyl ?"); \
+              printf("reponse 1 : "); \
+              printf("reponse 2 : "); \
+              printf("reponse 3 : "); \
+              printf("reponse 4 : "); \
+              printf("reponse 5 : "); \
+              printf("reponse 6 : "); \
+              printf("reponse 7 : "); \
+              printf("reponse 8 : "); \
+              printf("reponse 9 : "); \
+              printf("reponse 10 : ");
+
+#define NQ7() printf("Quel est la définition exacte d'un ver informatique ?"); \
+              printf("reponse 1 : "); \
+              printf("reponse 2 : "); \
+              printf("reponse 3 : "); \
+              printf("reponse 4 : "); \
+              printf("reponse 5 : "); \
+              printf("reponse 6 : "); \
+              printf("reponse 7 : "); \
+              printf("reponse 8 : "); \
+              printf("reponse 9 : "); \
+              printf("reponse 10 : ");
+
+#define NQ8() printf("Quel est le but d'un logiciel espion"); \
+              printf("reponse 1 : "); \
+              printf("reponse 2 : "); \
+              printf("reponse 3 : "); \
+              printf("reponse 4 : "); \
+              printf("reponse 5 : "); \
+              printf("reponse 6 : "); \
+              printf("reponse 7 : "); \
+              printf("reponse 8 : "); \
+              printf("reponse 9 : "); \
+              printf("reponse 10 : ");
+
+#define NQ9() printf("Qu'es-ce qu'un payload ?"); \
+              printf("reponse 1 : "); \
+              printf("reponse 2 : "); \
+              printf("reponse 3 : "); \
+              printf("reponse 4 : "); \
+              printf("reponse 5 : "); \
+              printf("reponse 6 : "); \
+              printf("reponse 7 : "); \
+              printf("reponse 8 : "); \
+              printf("reponse 9 : "); \
+              printf("reponse 10 : ");
+
+#define NQ10() printf("Quel est le but d'un wiper (logiciel malveillant) ?"); \
+               printf("reponse 1 : "); \
+               printf("reponse 2 : "); \
+               printf("reponse 3 : "); \
+               printf("reponse 4 : "); \
+               printf("reponse 5 : "); \
+               printf("reponse 6 : "); \
+               printf("reponse 7 : "); \
+               printf("reponse 8 : "); \
+               printf("reponse 9 : "); \
+               printf("reponse 10 : ");
 
 #define PRENOM() printf("Quel est votre prenom : ");
 
