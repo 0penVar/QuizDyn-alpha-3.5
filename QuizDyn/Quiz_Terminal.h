0dddbe8a9d277c1de295fab4158f5b570df2f5ba
0penVar <71385626+0penVar@users.noreply.github.com>
2021-05-25 16:53:13 -0400

    Update Quiz_Terminal.h

--- QuizDyn/Quiz_Terminal.h
+++ QuizDyn/Quiz_Terminal.h
@@ -1,9 +1,9 @@
-#ifndef
+#ifndef QUIZ_TERMINAL_H_INCLUDED
+#define QUIZ_TERMINAL_H_INCLUDED
 
 
 #define CLS() system("CLS");
 #define END_TASK() system("taskkill /IM cb_console_runner.exe");
-#define 
 
 
 #endif
