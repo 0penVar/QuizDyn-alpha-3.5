163c1969e655b8cb2ec51530571c89009e18a472
0penVar <71385626+0penVar@users.noreply.github.com>
2021-05-27 00:31:34 -0400

    Update Quiz_Terminal.h

--- QuizDyn/Quiz_Terminal.h
+++ QuizDyn/Quiz_Terminal.h
@@ -3,7 +3,10 @@
 
 
 #define CLS() system("CLS");
+
 #define END_TASK() system("taskkill /IM cb_console_runner.exe");
+
+#define WRITE_HERE() printf("Veuillez ecrire votre reponse ci-contre : ");
 
 
 #endif
